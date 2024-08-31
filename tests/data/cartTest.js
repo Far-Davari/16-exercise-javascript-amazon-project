89f59039da0f9655c9f30a1d77d6605e01b18ac2
Farshad Davari <DavariFarshad3@outlook.com>
2024-08-31 10:15:57 +0330

    Test updateDeliveryOption.

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,9 @@
-import { addToCart, cart, loadFromStorage, removeFromCart } from "../../data/cart.js";
+import {
+  addToCart,
+  cart,
+  loadFromStorage, 
+  removeFromCart, 
+  updateDeliveryOption } from "../../data/cart.js";
 
 describe("test suite: addToCart", () => {
   beforeEach(() => {
@@ -92,6 +97,37 @@ describe("test suit: removeFromCart", () => {
       productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity:1,
       deliveryOptionId:"1"
+    }]));
+  });
+});
+
+describe("test suit: updateDeliveryOption", () => {
+  beforeEach(() => {
+    // prevent test suit to modify our code:
+    spyOn(localStorage, "setItem");
+  });
+
+  it("update the delivery option", () => {
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 1, 
+        deliveryOptionId: "1"
+      }]);
+    });
+    loadFromStorage();
+
+    updateDeliveryOption("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", "3");
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cart[0].quantity).toEqual(1);
+    expect(cart[0].deliveryOptionId).toEqual("3");
+
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith("cart", JSON.stringify([{
+      productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity:1,
+      deliveryOptionId:"3"
     }]));
   });
 });
